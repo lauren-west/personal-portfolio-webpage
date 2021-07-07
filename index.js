b6a32f77538ab8aa8f2824617ecbc9a3bcd4d376
lauren-west <lwest@hmc.edu>
2021-07-06 23:43:39 -0700

    dynamic!!!

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function changeColor(newColor) { 
-    document.getElementById('coffee').style.color = newColor; 
+function changeCoffee() { 
+    if (document.getElementById('coffee').innerHTML == "Or perhaps tea, just for you?"){
+        document.getElementById('coffee').innerHTML = "How do you like your coffee?"
+        document.getElementById('coffee').style.color = 'white';
+    } else {
+        document.getElementById('coffee').innerHTML = "Or perhaps tea, just for you?"
+        document.getElementById('coffee').style.color = "#4E2E54";
+    }
 }
